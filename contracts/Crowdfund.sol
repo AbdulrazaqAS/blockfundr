85a0271ba13a71158a8f710ac17477999856aa5e
Abdulrazaq Ahmad Sabo <abdulrazaq2621@gmail.com>
2025-03-12 23:01:29 +0100

    snapshot

---
+++ contracts/Crowdfund.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract Crowdfund {
+    struct Campaign {
+        address creator;
+        string metadataUrl;
+        uint256 goal;
+        uint256 deadline;
+        uint256 fundsRaised;
+        bool goalReached;
+        mapping(address => uint256) contributions;
+    }
+
+    uint public campaignCount;
+    mapping(uint => Campaign) public campaigns;
+
+    uint256 public MIN_GOAL = 0.0005 ether;
+    uint256 public MIN_DURATION = 1 days;
+
+    event CampaignCreated(uint indexed campaignId, address creator, uint goal, uint deadline);
+    event Funded(uint indexed campaignId, address backer, uint amount);
+    event Withdrawn(uint indexed campaignId, address creator, uint amount);
+    event Refunded(uint indexed campaignId, address backer, uint amount);
+
+    function createCampaign(string memory _metadataUrl, uint _goal, uint _duration) external {
+        require(_goal > MIN_GOAL, "Goal must be greater than MIN_GOAL");
+        require(_duration > MIN_DURATION, "Duration must be greater than MIN_DURATION");
+
+        campaignCount++;
+        Campaign storage campaign = campaigns[campaignCount];
+        campaign.creator = msg.sender;
+        campaign.metadataUrl = _metadataUrl;
+        campaign.goal = _goal;
+        campaign.deadline = block.timestamp + _duration;
+
+        emit CampaignCreated(campaignCount, msg.sender, _goal, campaign.deadline);
+    }
+}
---
+++ test/Crowdfund.js
@@ -0,0 +1,44 @@
+//const hre = require("hardhat");
+const {expect, assert} = require("chai");
+const {loadFixture} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+
+describe("Crowdfund", ()=>{
+	async function deployContractFixture(){
+		let [owner] = await ethers.getSigners();
+
+		const CrownFund = await ethers.getContractFactory("Crowdfund");
+		const crowdfund = await CrownFund.deploy();
+
+		return { owner, crowdfund };
+	}
+
+	describe ("Deployment", ()=>{
+		const metadataUrl = "https://cominglater.com/json"
+		const goal = ethers.parseEther("10");
+		const duration = 24 * 60 * 60 * 1000  // seconds
+
+		it ("Should emit create event", async ()=>{
+			const { crowdfund } = await loadFixture(deployContractFixture);
+			expect(await crowdfund.createCampaign(metadataUrl, goal, duration))
+						.to.emit(crowdfund, "CampaignCreated");
+		});
+
+		it ("Should increment campaigns count", async ()=>{
+			const { crowdfund } = await loadFixture(deployContractFixture);
+			const count1 = await crowdfund.campaignCount();
+			await crowdfund.createCampaign(metadataUrl, goal, duration)
+			const count2 = await crowdfund.campaignCount();
+			assert(count2 - count1 === 1, "");
+		});
+
+		// it("Should fail if duration is less than MIN_DURATION", async function () {
+	    //   await expect(await crowdfund.createCampaign(metadataUrl, goal, duration))
+	    //   	.to.be.revertedWith(
+	    //     	"Duration must be greater than MIN_DURATION"
+	    //   );
+
+		// it ("Should increment count", async ()=>{
+		// 	expect(crowdfund.campaignCount).gr()
+		// });
+	});
+});
